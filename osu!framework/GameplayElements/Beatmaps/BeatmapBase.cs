323e250acce7d7726d90ddc3cc8887bfa98a912d
Dean Herbert <pe@ppy.sh>
2013-05-08 04:41:27 +0800

    Added OsuHelper static class used to talk directly to osu! outside of provided AiMod endpoints. Initially allows retrieval of the active Beatmap.

---
+++ osu!framework/GameplayElements/Beatmaps/BeatmapBase.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace osu.GameplayElements.Beatmaps
+{
+    public abstract class BeatmapBase : MarshalByRefObject
+    {
+        #region Difficulty Settings
+        public byte DifficultyApproachRate = 5;
+        public byte DifficultyCircleSize = 5;
+        public byte DifficultyHpDrainRate = 5;
+        public byte DifficultyOverall = 5;
+
+        public double DifficultySliderMultiplier = 1.4;
+        public double DifficultySliderTickRate = 1;
+        #endregion
+
+        #region Metadata
+        public string Artist = string.Empty;
+        public string ArtistUnicode;
+
+        public string Tags = string.Empty;
+        public string Title = string.Empty;
+        public string TitleUnicode;
+        #endregion
+    }
+}
---
+++ osu!framework/Interfacing/OsuHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using osu.GameplayElements.Beatmaps;
+
+namespace osu.Interfacing
+{
+    public abstract class OsuHelper
+    {
+        public static BeatmapBase GetCurrentBeatmap()
+        {
+            return instance.getCurrentBeatmap();
+        }
+
+        #region Internal osu! connection handling
+        protected static OsuHelper instance;
+
+        abstract protected BeatmapBase getCurrentBeatmap();
+        #endregion
+    }
+}
