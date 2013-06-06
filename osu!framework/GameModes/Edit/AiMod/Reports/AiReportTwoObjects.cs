f74a83ee8eb33ce74e8d5ee7f0a70e52f5061e95
Dean Herbert <pe@ppy.sh>
2013-06-06 17:11:26 +0900

    .net4.5

--- osu!framework/GameModes/Edit/AiMod/Reports/AiReportTwoObjects.cs
+++ osu!framework/GameModes/Edit/AiMod/Reports/AiReportTwoObjects.cs
@@ -12,11 +12,11 @@ namespace osu.GameModes.Edit.AiMod.Reports
         public readonly HitObjectBase h2;
 
         public AiReportTwoObjects(HitObjectBase h1, HitObjectBase h2, BeenCorrectedDelegate corrected, Severity severity, string information, int weblink)
-            : base((h1.EndTime - h2.StartTime) / 2 + h2.StartTime, severity, information, weblink,corrected)
+            : base((h1.EndTime - h2.StartTime) / 2 + h2.StartTime, severity, information, weblink, corrected)
         {
             this.h1 = h1;
             this.h2 = h2;
-            
+
             RelatedHitObjects.Add(h1);
             RelatedHitObjects.Add(h2);
         }
